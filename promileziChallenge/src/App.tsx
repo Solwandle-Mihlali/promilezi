d992d52901e96837e35e67f3dea0b12c93f2be7c
Mihlali Solwandle <mihlalisolwandle705@gmail.com>
2024-02-29 03:52:10 +0200

    design done

--- promileziChallenge/src/App.tsx
+++ promileziChallenge/src/App.tsx
@@ -1,6 +1,8 @@
 
 import './App.css'
+import Footer from './components/footer'
 import NavBar from './components/navBar'
+import Solutions from './components/solutions'
 import Welcome from './components/welcome'
 import WhySection from './components/whySection'
 
@@ -14,9 +16,19 @@ function App() {
      <Welcome/>
      <div className='splitter'>
       <span></span>
-      <p>WHY<span>ProSuite</span></p>
+      <p>Why<span>ProSuite</span></p>
      </div>
      <WhySection/>
+     <div className='splitter'>
+      <span></span>
+      <p>Our<span>Solutions</span></p>
+     </div>
+     <Solutions/>
+     <div className="coloredSplitter">
+      <p>We'd love to show you how our solution works</p>
+      <button>REQUEST A DEMO</button>
+     </div>
+     <Footer/>
 
       
     </>
--- promileziChallenge/src/assets/images.tsx
+++ promileziChallenge/src/assets/images.tsx
@@ -1,11 +1,13 @@
 import logo from "../assets/logo.png";
 import laptop from "../assets/laptop.png";
 import toggleIcon from "../assets/toggleIcon.png"
+import whiteLogo from "./logo white.png"
 
 
 export const images = {
 
     logo : logo ,
     laptop : laptop,
-    toggleIconn : toggleIcon
+    toggleIconn : toggleIcon,
+    logoWhite: whiteLogo
 }
